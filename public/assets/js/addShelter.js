c7919d7863c7d5fc691fc96be4a3a91ae6739e1e
Philip Hurst <youngphil9@gmail.com>
2020-05-06 13:49:52 -0500

    clears form inputs after added

--- public/assets/js/addShelter.js
+++ public/assets/js/addShelter.js
@@ -4,29 +4,22 @@ $(function () {
     const dogName = $("#dog-name");
     const dogBreed = $("#dog-breed");
 
-
-
     $("#find-add").on("click", function (e) {
         e.preventDefault();
         const shelter = {
             name: shelterName.val().trim(),
             city: shelterCity.val().trim(),
         };
-        // console.log(shelterName);
-        // console.log(shelter);
         if (shelterName.val() === "" || shelterCity.val() === "") {
-            // console.log('cant do that')
             $("#shelter-error").append($("<p>").text("Shelter name and city cannot be blank!"));
             setTimeout(function () {
                 $("#shelter-error").empty();
-            }, 3000);
+            }, 5000);
         } else {
             $.ajax("/api/shelters", {
                 type: "POST",
                 data: shelter
             }).then(function (res) {
-                // console.log(res);
-
                 const id = res.id;
 
                 $.ajax("/api/shelters/" + id, {
@@ -57,25 +50,21 @@ $(function () {
             $("#dog-error").append($("<p>").text("All fields must have a value!"));
             setTimeout(function () {
                 $("#dog-error").empty();
-            }, 3000);
+            }, 5000);
         } else {
             $.ajax("/api/shelterDogs", {
                 type: "POST",
                 data: dog
             }).then(function (res) {
-                // location.reload();
                 const id = res.id;
-                // console.log(res);
                 if (res.id > 0) {
                     $.ajax("/api/shelters/" + id, {
                         type: "GET"
                     }).then(function (data) {
-                        // dogName.val().empty();
-                        // dogBreed.val().empty();
-                        // dogName.reset();
-                        // dogBreed.val() = "";
                         $("#title").empty();
                         $("#table-body").empty();
+                        document.getElementById("dog-name").value = "";
+                        document.getElementById("dog-breed").value = "";
 
                         $("#title").append($("<h3>").text(`${data.name} in ${data.city}`));
                         for (let i = 0; i < data.ShelterDogs.length; i++) {
@@ -86,7 +75,7 @@ $(function () {
                     $("#dog-error").append($("<p>").text("Shelter not found, must create one first!"));
                     setTimeout(function () {
                         $("#dog-error").empty();
-                    }, 3000);
+                    }, 5000);
                 }
             });
         }
